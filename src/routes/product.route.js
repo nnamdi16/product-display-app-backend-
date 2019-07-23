28d44c40631793a4e70264b67e9ce1e6e30534c5
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-07-23 10:15:03 +0100

    Create router for getting a particular product, updating a particular product and deleting a product

---
+++ src/routes/product.route.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { celebrate: validate } = require("celebrate");
+const productCtrl = require('../controllers/product.controller');
+const { createProduct, updateProduct, getProduct } = require('../validation/product.validation');
+const upload = require('../utils/file-upload');
+
+const router = express.Router();
+
+router
+  .route('/')
+  .get(productCtrl.getAll)
+  .post(upload('images').single('image'), validate(createProduct, { abortEarly: false }), productCtrl.create);
+
+router
+  .route("/:id")
+  .get(validate(getProduct, { abortEarly: false }), productCtrl.getOne)
+  .put(upload('images').single('image'), validate(updateProduct, { abortEarly: false }), productCtrl.update)
+  .delete(productCtrl.remove);
+
+module.exports = router;
