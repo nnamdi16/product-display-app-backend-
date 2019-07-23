b0a4bd5c60a7bd9be5158e9e6fae9c91a1237043
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-07-23 10:12:25 +0100

    Create schema for the product

---
+++ src/model/Product.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  color: {
+    type: String,
+    required: true
+  }
+});
+
+exports.Product = mongoose.model("Product", ProductSchema);
