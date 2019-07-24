2fbaf29305a6a3b976293c216596d675c5443b3e
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-07-24 18:25:05 +0100

    File format to text file for file upload

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,20 +1,28 @@
-const express = require('express');
+const express = require("express");
 const { celebrate: validate } = require("celebrate");
-const productCtrl = require('../controllers/product.controller');
-const { createProduct, updateProduct, getProduct } = require('../validation/product.validation');
-const upload = require('../utils/file-upload');
+const productCtrl = require("../controllers/product.controller");
+const {
+  createProduct,
+  updateProduct,
+  getProduct
+} = require("../validation/product.validation");
+const upload = require("../utils/file-upload");
 
 const router = express.Router();
 
 router
-  .route('/')
+  .route("/")
   .get(productCtrl.getAll)
-  .post(upload('images').single('image'), validate(createProduct, { abortEarly: false }), productCtrl.create);
+  .post(
+    // upload("images").single("image"),
+    validate(createProduct, { abortEarly: false }),
+    productCtrl.create
+  );
 
 router
   .route("/:id")
   .get(validate(getProduct, { abortEarly: false }), productCtrl.getOne)
-  .put(upload('images').single('image'), validate(updateProduct, { abortEarly: false }), productCtrl.update)
+  .put(validate(updateProduct, { abortEarly: false }), productCtrl.update)
   .delete(productCtrl.remove);
 
 module.exports = router;
