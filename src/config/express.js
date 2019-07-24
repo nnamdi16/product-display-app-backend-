83c351b80ef1fc55a46e67a47d6f04625c8456b8
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-07-24 18:22:57 +0100

    Add file size limit

--- src/config/express.js
+++ src/config/express.js
@@ -1,18 +1,17 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const methodOverride = require("method-override");
 const helmet = require("helmet");
 
-const routes = require('../routes');
+const routes = require("../routes");
 
 const app = express();
 
 // parse body params and attache them to req.body
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cookieParser());
 app.use(methodOverride());
@@ -23,11 +22,18 @@ app.use(helmet());
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors());
 
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+// });
+
 // mount all routes on /api path
-app.use('/api/v1', routes);
+app.use("/api/v1", routes);
 app.get("/", (req, res) => {
   res.json("Hello world");
 });
-
 
 module.exports = app;
